23840b22d6f8d92ec2728e47a12f499cd0854272
M@CK <mangeshpatil619@gmail.com>
2019-02-08 21:21:50 +0530

    Initial Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    public static WebDriver driver;
+
+
+    @BeforeSuite
+    public void setUp(){
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\MACK\\Documents\\TestSoftware\\geckodriver.exe");
+        driver = new FirefoxDriver();
+
+    }
+
+    @Test
+    public void doLogin() {
+        driver.get("http://gmail.com");
+        driver.findElement(By.id("identifierId")).sendKeys("mangeshpatil619@gmail.com");
+        //driver.findElement(By.id("Passwd")).sendKeys("shaa@123");
+
+    }
+
+    @AfterSuite
+    public void tearDown(){
+
+        driver.quit();
+    }
+}
