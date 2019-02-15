f08bff4e334b228d2d45aaf62dbc64e22a35e217
M@CK <mangeshpatil619@gmail.com>
2019-02-15 19:29:26 +0530

    Third commit :->
    Changing Firefox To Chrome Test Purpose.

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,6 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -13,8 +13,8 @@ public class LoginTest {
     @BeforeSuite
     public void setUp(){
 
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\MACK\\Documents\\TestSoftware\\geckodriver.exe");
-        driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\MACK\\Documents\\TestSoftware\\ChromeDriver.exe");
+        driver = new ChromeDriver();
 
     }
 
@@ -22,7 +22,7 @@ public class LoginTest {
     public void doLogin() {
         driver.get("http://gmail.com");
         driver.findElement(By.id("identifierId")).sendKeys("mangeshpatil619@gmail.com");
-        driver.findElement(By.id("Passwd")).sendKeys("shaa@123");
+      //  driver.findElement(By.id("Passwd")).sendKeys("shaa@123");
 
 
     }
